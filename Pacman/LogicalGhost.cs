171cf85e80a4ee5de73ce14fdfb553dd3449c7d3
Yannick Gähwiler <yannick.gaehwiler@six-group.com>
2017-12-27 15:21:15 +0100

    Refactoring - LogicalGhost

--- Pacman/LogicalGhost.cs
+++ Pacman/LogicalGhost.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using Pacman.Maze;
 using Pacman.Pacman;
 
@@ -15,18 +14,15 @@ namespace Pacman
 
         private readonly LogicalPacman _logicalPacman;
 
-        private Direction _dir;
+        
 
-        private double _distance;
-        private double _d1;
-        private double _d2;
-        private double _d3;
-        private double _d4;
+        private double _distanceToPacman;
+        
 
         public LogicalGhost(int row, int col, ILogicalMaze logicalMaze, LogicalPacman logicalPacman) : base(col, row, logicalMaze)
         {
             this._logicalPacman = logicalPacman;
-            GetDistanceOnce();
+            _distanceToPacman = GetDistanceToPacman();
 
             _movementMapping = new Dictionary<int, Direction>
             {
@@ -43,74 +39,47 @@ namespace Pacman
 
             Move(movementDelta.Item1, movementDelta.Item2);
 
-            Distance();
-            ChangeDirection(_dir);
+            ChangeDirection(GetDirection());
+        }
 
+        private Direction GetDirection()
+        {
+            var newDirection = Direction.Up;
+            var distanceRight = GetDistanceToPacman(rowDelta: 0, columnDelta: 1);
+            var distanceLeft = GetDistanceToPacman(rowDelta: 0, columnDelta: -1);
+            var distanceDown = GetDistanceToPacman(rowDelta: +1, columnDelta: 0);
+            var distanceUp = GetDistanceToPacman(rowDelta: -1, columnDelta: 0);
+
+            if (distanceRight <= _distanceToPacman && this._logicalMaze.Field[this.Row, this.Column + 1] != MazeTile.Wall)
+            {
+                _distanceToPacman = distanceRight;
+                newDirection = Direction.Right;
+            }
+            if (distanceLeft <= _distanceToPacman && this._logicalMaze.Field[this.Row, this.Column - 1] != MazeTile.Wall)
+            {
+                _distanceToPacman = distanceLeft;
+                newDirection = Direction.Left;
+            }
+            if(distanceDown <= _distanceToPacman && this._logicalMaze.Field[this.Row + 1, this.Column] != MazeTile.Wall)
+            {
+                _distanceToPacman = distanceDown;
+                newDirection = Direction.Down;
+            }
+            if(distanceUp <= _distanceToPacman && this._logicalMaze.Field[this.Row - 1, this.Column] != MazeTile.Wall)
+            {
+                _distanceToPacman = distanceUp;
+                newDirection = Direction.Up;
+            }
+
+            _distanceToPacman = GetDistanceToPacman();
+            return newDirection;
+        }
+
+        private double GetDistanceToPacman(int rowDelta = 0, int columnDelta = 0)
+        {
+            var p1 = Math.Pow(this.Row + rowDelta - _logicalPacman.Row, 2);
+            var p2 = Math.Pow(this.Column + columnDelta - _logicalPacman.Column, 2);
             
-        }
-
-        public void Distance()
-        {
-            Console.WriteLine("-----------");
-            this.Column++;
-            _d1 = GetDistance();
-            Console.WriteLine(_d1);
-            this.Column--;
-
-            this.Column--;
-            _d2 = GetDistance();
-            Console.WriteLine(_d2);
-            this.Column++;
-
-            this.Row++;
-            _d3 = GetDistance();
-            Console.WriteLine(_d3);
-            this.Row--;
-
-            this.Row--;
-            _d4 = GetDistance();
-            Console.WriteLine(_d4);
-            this.Row++;
-
-            if (_d1 <= _distance && this._logicalMaze.Field[this.Row, this.Column + 1] != MazeTile.Wall)
-            {
-                _distance = _d1;
-                _dir = Direction.Right;
-                
-            }
-            if (_d2 <= _distance && this._logicalMaze.Field[this.Row, this.Column - 1] != MazeTile.Wall)
-            {
-                _distance = _d2;
-                _dir = Direction.Left;
-            }
-            if(_d3 <= _distance && this._logicalMaze.Field[this.Row + 1, this.Column] != MazeTile.Wall)
-            {
-                _distance = _d3;
-                _dir = Direction.Down;
-            }
-            if(_d4 <= _distance && this._logicalMaze.Field[this.Row - 1, this.Column] != MazeTile.Wall)
-            {
-                _distance = _d4;
-                _dir = Direction.Up;
-            }
-            GetDistanceOnce();
-            Console.WriteLine("DISTANCE: " + _distance);
-            Console.WriteLine(_dir);
-        }
-
-        public void GetDistanceOnce()
-        {
-            var p1 = Math.Pow(this.Row - _logicalPacman.Row, 2);
-            var p2 = Math.Pow(this.Column - _logicalPacman.Column, 2);
-
-            _distance = Math.Sqrt(p1 + p2);
-        }
-
-        public double GetDistance()
-        {
-            var p1 = Math.Pow(this.Row - _logicalPacman.Row, 2);
-            var p2 = Math.Pow(this.Column - _logicalPacman.Column, 2);
-
             return Math.Sqrt(p1 + p2);
         }
     }
